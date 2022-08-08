b57c7921230a3cff0e6452dbb35887ad2e98308b
cooklee <sbbogus@gmail.com>
2022-08-08 11:03:00 +0200

    models

---
+++ filmy/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FilmyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'filmy'
---
+++ filmy/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+class Category(models.Model):
+    name = models.CharField(max_length=125)
+
+class Film(models.Model):
+    title = models.CharField(max_length=128)
+    year = models.IntegerField()
+    director = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='directed')
+    screenwriter = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='writed')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
